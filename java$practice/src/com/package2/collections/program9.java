24e457aad98732e53452a763c675d7cea323227c
nitha01 <nitha01sa@gmail.com>
2022-01-14 19:00:22 -0800

    update

--- java$practice/src/com/package2/collections/program9.java
+++ java$practice/src/com/package2/collections/program9.java
@@ -1,5 +1,29 @@
 package com.package2.collections;
 
-public class program9 {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
+public class program9 {
+    //Write a program to sort HashMap by keys
+    public static void main(String args[]) {
+        HashMap<Integer,String> hm = new HashMap<>();
+        hm.put(42,"Rani");
+        hm.put(30, "Joy");
+        hm.put(14, "Merry");
+        hm.put(38, "Mike");
+        System.out.println(hm);
+        System.out.println("==================Before Sorting==============");
+        for(Map.Entry<Integer,String>e :hm.entrySet()){
+            System.out.println(e.getKey()+":"+e.getValue());
+        }
+        System.out.println("================After Sorting==============");
+        TreeMap<Integer,String> treemap = new TreeMap<>();
+        // Copy all data from hashMap into TreeMap
+        treemap.putAll(hm);
+        for(Map.Entry<Integer,String>e :treemap.entrySet()){
+            System.out.println(e.getKey()+":"+e.getValue());
+        }
+
+    }
 }
